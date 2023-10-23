16a0a3f2dbf9423e7eab19677d8321d5ba5055b4
Oksana <oksana.s.kovalchuk@gmail.com>
2023-10-23 12:57:10 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 import { App } from './App'
 
