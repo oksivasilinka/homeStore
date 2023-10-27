3c4ffcc23f46c9df9e9d0fd8ac9b7dbacff18329
Oksana <oksana.s.kovalchuk@gmail.com>
2023-10-27 16:18:20 +0300

    feat: add product card in cart

---
+++ src/components/productInCart/productCardInCart.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ProductCardInCart } from './productCardInCart'
+
+const meta = {
+  component: ProductCardInCart,
+  tags: ['autodocs'],
+  title: 'Components/ProductCardInCart',
+} satisfies Meta<typeof ProductCardInCart>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const ProductInCartStory: Story = {
+  args: {
+    product: {
+      description: 'Очень модный желтый диван. Из самой новой коллекции',
+      id: '1',
+      name: 'Диван желтый',
+      photo:
+        'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTIfwXeUDy8Y_EUi8qQBrZAuSIz38EoKXVSoUUUM5dsC7k66BdnsFE0-qM7nHCt_tv5UdISqadirbax3OEn67Tga5IvKxwMcljcJyl9R3JdQBfICxuY-KQDvDBPVYpOmKWXeIz-_Q&usqp=CAc',
+      price: 859,
+    },
+  },
+}
---
+++ src/components/productInCart/productCardInCart.tsx
@@ -0,0 +1,30 @@
+import { Product } from '@/components/productCardsList/productCard'
+import { Card, CardActions } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+
+type Props = {
+  product: Product
+}
+export const ProductCardInCart = ({ product }: Props) => {
+  const { description, name, photo, price } = product
+
+  return (
+    <Card sx={{ display: 'flex' }}>
+      <CardMedia component={'img'} image={photo} sx={{ width: 200 }} title={name} />
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent>
+          <Typography variant={'h6'}>{name}</Typography>
+          <Typography variant={'body2'}>{description}</Typography>
+          <Typography variant={'h6'}>Цена {price} руб.</Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant={'outlined'}>Купить</Button>
+        </CardActions>
+      </Box>
+    </Card>
+  )
+}
