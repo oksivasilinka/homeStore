eb219dd953e41e6d310b6e8ca484cbc8a0b5b185
Oksana <oksana.s.kovalchuk@gmail.com>
2023-12-26 16:59:27 +0300

    refactor

---
+++ src/components/productInCart/numericInput/numericInput.tsx
@@ -0,0 +1,38 @@
+import { ProductInCart, addProductInCart, deleteProductInCart } from '@/services/slice.ts'
+import { useAppDispatch } from '@/services/store.ts'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+import { IconButton } from '@mui/material'
+import TextField from '@mui/material/TextField'
+
+import s from './numericInput.module.scss'
+
+type Props = {
+  product: ProductInCart
+}
+
+export const NumericInput = ({ product }: Props) => {
+  const dispatch = useAppDispatch()
+
+  const productCount = product.totalCount
+
+  const addProductHandler = () => {
+    dispatch(addProductInCart({ product }))
+  }
+
+  const deleteProductHandler = () => {
+    dispatch(deleteProductInCart({ product }))
+  }
+
+  return (
+    <>
+      <IconButton onClick={deleteProductHandler}>
+        <RemoveIcon />
+      </IconButton>
+      <TextField className={s.input} size={'small'} value={productCount} />
+      <IconButton onClick={addProductHandler}>
+        <AddIcon />
+      </IconButton>
+    </>
+  )
+}
--- src/components/productInCart/productCardInCart.tsx
+++ src/components/productInCart/productCardInCart.tsx
@@ -1,12 +1,9 @@
-import { ProductInCart, addProductInCart, deleteProductInCart } from '@/services/slice'
-import { useAppDispatch } from '@/services/store'
-import AddIcon from '@mui/icons-material/Add'
-import RemoveIcon from '@mui/icons-material/Remove'
-import { Card, CardActions, IconButton } from '@mui/material'
+import { NumericInput } from '@/components/productInCart/numericInput'
+import { ProductInCart } from '@/services/slice'
+import { Card, CardActions } from '@mui/material'
 import Box from '@mui/material/Box'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
 import s from './productCardInCart.module.scss'
@@ -15,18 +12,7 @@ type Props = {
   product: ProductInCart
 }
 export const ProductCardInCart = ({ product }: Props) => {
-  const { name, photo, price, totalCount, totalSum } = product
-  const dispatch = useAppDispatch()
-
-  const productCount = totalCount
-
-  const addProductHandler = () => {
-    dispatch(addProductInCart({ product }))
-  }
-
-  const deleteProductHandler = () => {
-    dispatch(deleteProductInCart({ product }))
-  }
+  const { name, photo, price, totalSum } = product
 
   return (
     <Card className={s.wrapper}>
@@ -41,13 +27,7 @@ export const ProductCardInCart = ({ product }: Props) => {
           <Typography variant={'h6'}>Сумма {totalSum} руб.</Typography>
         </CardContent>
         <CardActions className={s.buttonWrapper}>
-          <IconButton onClick={deleteProductHandler}>
-            <RemoveIcon />
-          </IconButton>
-          <TextField className={s.input} size={'small'} value={productCount} />
-          <IconButton onClick={addProductHandler}>
-            <AddIcon />
-          </IconButton>
+          <NumericInput product={product} />
         </CardActions>
       </Box>
     </Card>
