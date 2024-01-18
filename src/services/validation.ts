804da274bc143af8e9d2c6b34526ba7258125672
Oksana <oksana.s.kovalchuk@gmail.com>
2024-01-18 11:49:08 +0300

    update validation schemas

--- src/services/validation.ts
+++ src/services/validation.ts
@@ -1,48 +1,41 @@
 import { object, ref, string } from 'yup'
 
-export const phoneRegExp =
+const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
+const emailSchema = string()
+  .required('Поле обязательно для заполнения')
+  .email('Неверный формат электронной почты')
+  .min(10, 'Минимум 10 символов')
+  .max(30, 'Не более 30 символов')
+
+const passwordSchema = string()
+  .required('Поле обязательно для заполнения')
+  .min(5, 'Минимум 5 символов')
+  .max(10, 'Не более 10 символов')
+
+const nameSchema = string()
+  .required('Поле обязательно для заполнения')
+  .min(3, 'Минимум 3 символа')
+  .max(30, 'Не более 30 символов')
+
+const phoneSchema = string()
+  .matches(phoneRegExp, 'Phone number is not valid')
+  .required('Поле обязательно для заполнения')
+
 export const formSchema = object({
-  email: string()
-    .required('Поле обязательно для заполнения')
-    .email('Неверный формат электронной почты')
-    .min(10, 'Минимум 10 символов')
-    .max(30, 'Не более 30 символов'),
-  name: string()
-    .required('Поле обязательно для заполнения')
-    .min(3, 'Минимум 3 символа')
-    .max(30, 'Не более 30 символов'),
-  phone: string()
-    .matches(phoneRegExp, 'Phone number is not valid')
-    .required('Поле обязательно для заполнения'),
+  email: emailSchema,
+  name: nameSchema,
+  phone: phoneSchema,
 })
 
 export const signInSchema = object({
-  email: string()
-    .required('Поле обязательно для заполнения')
-    .email('Неверный формат электронной почты')
-    .min(10, 'Минимум 10 символов')
-    .max(30, 'Не более 30 символов'),
-  password: string()
-    .required('Поле обязательно для заполнения')
-    .min(5, 'Минимум 5 символов')
-    .max(10, 'Не более 10 символов'),
+  email: emailSchema,
+  password: passwordSchema,
 })
 
 export const loginSchema = object({
-  confirmPassword: string()
-    .required('Поле обязательно для заполнения')
-    .min(5, 'Минимум 5 символов')
-    .max(10, 'Не более 10 символов')
-    .oneOf([ref('password')], 'Пароли не совпадают'),
-  email: string()
-    .required('Поле обязательно для заполнения')
-    .email('Неверный формат электронной почты')
-    .min(10, 'Минимум 10 символов')
-    .max(30, 'Не более 30 символов'),
-  password: string()
-    .required('Поле обязательно для заполнения')
-    .min(5, 'Минимум 5 символов')
-    .max(10, 'Не более 10 символов'),
+  confirmPassword: passwordSchema.oneOf([ref('password')], 'Пароли не совпадают'),
+  email: emailSchema,
+  password: passwordSchema,
 })
