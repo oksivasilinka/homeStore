a249a12c704a5234f0e7eb8afeedba08a8b87525
Oksana <oksana.s.kovalchuk@gmail.com>
2024-01-21 15:24:51 +0300

    update catalog story

--- src/pages/catalog/catalog.stories.tsx
+++ src/pages/catalog/catalog.stories.tsx
@@ -10,7 +10,7 @@ const meta = {
 
 export default meta
 type Story = StoryObj<typeof meta>
-export const ProductCardsListStory: Story = {
+export const CatalogStory: Story = {
   args: {
     isAuth: true,
     pageSize: 9,
