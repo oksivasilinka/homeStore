d11a2f6da173dd542f9da4455a00add7cb0569d3
Oksana <oksana.s.kovalchuk@gmail.com>
2024-01-21 15:49:27 +0300

    add numericInput story

---
+++ src/components/numericInput/numericInput.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { NumericInput } from '@/components'
+
+const meta = {
+  component: NumericInput,
+  tags: ['autodocs'],
+  title: 'Components/NumericInput',
+} satisfies Meta<typeof NumericInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const NumericInputStory: Story = {
+  args: {
+    product: {
+      category: 'cushioned',
+      description: 'Очень модный желтый диван. Из самой новой коллекции',
+      id: '1',
+      name: 'Диван желтый',
+      photo:
+        'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTIfwXeUDy8Y_EUi8qQBrZAuSIz38EoKXVSoUUUM5dsC7k66BdnsFE0-qM7nHCt_tv5UdISqadirbax3OEn67Tga5IvKxwMcljcJyl9R3JdQBfICxuY-KQDvDBPVYpOmKWXeIz-_Q&usqp=CAc',
+      price: 859,
+      totalCount: 1,
+      totalSum: 859,
+    },
+  },
+}
