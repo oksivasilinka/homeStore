e1a6d038956a692d8ceedc0f1f6885ce4cef0a52
Oksana <oksana.s.kovalchuk@gmail.com>
2024-01-21 15:56:55 +0300

    add modals story

---
+++ src/components/modal/baseModal/baseModal.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { BaseModal, ConfirmOrder, InfoConfirm } from '@/components'
+
+const meta = {
+  component: BaseModal,
+  tags: ['autodocs'],
+  title: 'Components/Modal',
+} satisfies Meta<typeof BaseModal>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const data = {
+  email: 'example@gmail.com',
+  name: 'Valera',
+  phone: '8-029-333-33-33',
+  totalSum: 1500,
+}
+export const ModalConfirmOrderStory: Story = {
+  args: {
+    isOpen: true,
+    callback: () => {},
+    children: <ConfirmOrder callback={() => {}} data={data} />,
+  },
+}
+
+export const ModalInfoConfirmStory: Story = {
+  args: {
+    isOpen: true,
+    callback: () => {},
+    children: <InfoConfirm callback={() => {}} />,
+  },
+}
