09ee80fcb6ab3041c422cb3ecf59054271faba59
Oksana <oksana.s.kovalchuk@gmail.com>
2024-01-23 12:49:07 +0300

    fix styles

--- src/pages/catalog/productCard/productCard.tsx
+++ src/pages/catalog/productCard/productCard.tsx
@@ -1,4 +1,4 @@
-import { addProductInCart } from '@/services/cart'
+import { addProductInCart, cartSelector } from '@/services/cart'
 import { useAppDispatch } from '@/services/store'
 import { ProductInCart } from '@/services/types'
 import { Card } from '@mui/material'
@@ -8,6 +8,8 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 
 import s from './productCard.module.scss'
+import { useSelector } from 'react-redux'
+import { NumericInput } from '@/components'
 
 type Props = {
   product: ProductInCart
@@ -15,11 +17,14 @@ type Props = {
 
 export const ProductCard = ({ product }: Props) => {
   const { description, name, photo, price } = product
+  const cart = useSelector(cartSelector)
   const dispatch = useAppDispatch()
 
   const addProductHandler = () => {
     dispatch(addProductInCart({ product }))
   }
+
+  const productInCart = cart.find(el => el.id === product.id)
 
   return (
     <Paper className={s.card} elevation={3}>
@@ -38,9 +43,13 @@ export const ProductCard = ({ product }: Props) => {
           <Typography color={'primary'} className={s.price} variant={'h6'}>
             Цена {price} руб.
           </Typography>
-          <Button onClick={addProductHandler} variant={'contained'}>
-            В корзину
-          </Button>
+          {!productInCart && (
+            <Button className={s.button} onClick={addProductHandler} variant={'contained'}>
+              В корзину
+            </Button>
+          )}
+
+          {productInCart && <NumericInput product={productInCart} />}
         </>
       </Card>
     </Paper>
